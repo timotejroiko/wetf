32b3c3441ead5e2d7097ac4305bdce74c9ec20b4
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2023-04-26 20:36:21 -0300

    improve build script

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,9 +1,18 @@
 const { readFileSync, writeFileSync, mkdirSync, readdirSync } = require("fs");
 const uglify = require("uglify-js");
-const uglifyOptions = { mangle: { properties: { reserved: ["Packer", "Unpacker", "pack", "unpack", "Wetf"] } } };
 const package = require("../package.json");
 const pkg = capitalize(package.name);
 const src = readdirSync("./src").map(x => ({ name: x.split(".")[0], content: readFileSync(`./src/${x}`, "utf-8") }));
+
+const options = {
+	output: {
+		comments: false,
+		beautify: true,
+		/** @type {*} */ indent_level: "\t"
+	},
+	mangle: false,
+	/** @type {false} */ compress: false,
+};
 
 makeUMD();
 makeESM();
@@ -12,30 +21,27 @@ makeCJS();
 function makeUMD() {
 	mkdirSync("./umd", { recursive: true });
 	const umd = `
-	(
-		function (global, factory) {
-			typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-			typeof define === 'function' && define.amd ? define(['exports'], factory) :
-			(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global));
+(
+	function (global, factory) {
+		typeof exports === "object" && typeof module !== "undefined" ? factory(exports) :
+		typeof define === "function" && define.amd ? define(["exports"], factory) :
+		(global = typeof globalThis !== "undefined" ? globalThis : global || self, factory(global));
+	}(
+		this, function (exports) {
+			"use strict";
+			const o = exports.${pkg} || (exports.${pkg} = {});
+%
 		}
-		(
-			this, (
-				function (exports) {
-					'use strict';
-					const o = exports.${pkg} || (exports.${pkg} = {})
-					%
-				}
-			)
-		)
-	);`;
+	)
+);`;
 	let combined = "";
 	src.forEach(({ name, content }) => {
 		const modified = content.slice(0, content.indexOf("module.exports"));
-		const code = `o.${capitalize(name)} = ${uglify.minify(modified, uglifyOptions).code}`;
+		const code = `o.${capitalize(name)} = ${modified}`;
 		combined += `${code}\n`;
-		writeFileSync(`./umd/${name}.min.js`, uglify.minify(umd.replace("%", code), uglifyOptions).code);
+		writeFileSync(`./umd/${name}.js`, uglify.minify(umd.replace("%", code), options).code);
 	});
-	writeFileSync(`./umd/${package.name}.min.js`, uglify.minify(umd.replace("%", combined), uglifyOptions).code);
+	writeFileSync(`./umd/${package.name}.js`, uglify.minify(umd.replace("%", combined), options).code);
 }
 
 function makeESM() {
@@ -43,7 +49,7 @@ function makeESM() {
 	const names = [];
 	src.forEach(({ name, content }) => {
 		const modified = content.slice(0, content.indexOf("module.exports")).replace("class ", "export class ");
-		writeFileSync(`./esm/${name}.js`, modified);
+		writeFileSync(`./esm/${name}.js`, uglify.minify(modified, options).code);
 		names.push(capitalize(name));
 	});
 	const index = 
